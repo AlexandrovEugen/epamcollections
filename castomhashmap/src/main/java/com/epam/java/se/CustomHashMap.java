41c76c75e7da15ac087308150667bac9533c5b8e
Евгений Александров < >
2017-03-27 20:15:38 +0300

    +remove

--- castomhashmap/src/main/java/com/epam/java/se/CustomHashMap.java
+++ castomhashmap/src/main/java/com/epam/java/se/CustomHashMap.java
@@ -3,44 +3,97 @@ package com.epam.java.se;
 
 import java.util.Collection;
 import java.util.Map;
+import java.util.Objects;
 import java.util.Set;
 
-public class CustomHashMap<K,V> implements Map<K,V> {
+
+public class CustomHashMap<K, V> implements Map<K, V> {
+
+    private final Entry<K, V>[] buckets;
+    private int size;
+
+    public CustomHashMap() {
+        buckets = new Entry[16];
+    }
+
+    public CustomHashMap(int CAPACITY) {
+        buckets = new Entry[CAPACITY];
+    }
 
     public int size() {
-        return 0;
+        return size;
     }
 
     public boolean isEmpty() {
-        return false;
+        return size() == 0;
     }
 
     public boolean containsKey(Object key) {
+        if (isEmpty()) {
+            return false;
+        }
+        for (int i = 0; i < size; i++) {
+            if (buckets[i].key.equals(key)) {
+                return true;
+            }
+        }
         return false;
     }
 
     public boolean containsValue(Object value) {
+        if (isEmpty()) {
+            return false;
+        }
+        for (int i = 0; i < size; i++) {
+            if (buckets[i].value.equals(value)) {
+                return true;
+            }
+        }
         return false;
     }
 
     public V get(Object key) {
+        Objects.requireNonNull(key);
+        for (int i = 0; i < size; i++) {
+            if (buckets[i].key.equals(key)) {
+                return buckets[i].value;
+            }
+        }
         return null;
     }
 
     public V put(K key, V value) {
+        Objects.requireNonNull(key);
+        buckets[size] = new Entry<>(key, value);
+        size = size + 1;
         return null;
     }
 
     public V remove(Object key) {
-        return null;
+        Objects.requireNonNull(key);
+        int moveIndex = -1;
+        int index = -1;
+        V removed = null;
+
+        for (int i = 0; i < size; i++) {
+            if (buckets[i].key.equals(key)) {
+                index = i;
+                moveIndex = size - index - 1;
+                removed = buckets[i].value;
+            }
+        }
+        if (moveIndex > 0) {
+            System.arraycopy(buckets, index + 1, buckets, index, moveIndex);
+        }
+        buckets[--size] = null;
+
+        return removed;
     }
 
     public void putAll(Map<? extends K, ? extends V> m) {
-
     }
 
     public void clear() {
-
     }
 
     public Set<K> keySet() {
@@ -51,7 +104,32 @@ public class CustomHashMap<K,V> implements Map<K,V> {
         return null;
     }
 
-    public Set<Entry<K, V>> entrySet() {
+    public Set<Map.Entry<K, V>> entrySet() {
         return null;
+    }
+
+    private class Entry<K, V> {
+
+        private final K key;
+        private V value;
+
+        private Entry<K, V> next = null;
+
+        private Entry(K key, V value) {
+            this.key = key;
+            this.value = value;
+        }
+
+        public boolean hasNext() {
+            return this.next != null;
+        }
+
+        public Entry<K, V> next() {
+            return next;
+        }
+
+        public void setNext(Entry<K, V> next) {
+            this.next = next;
+        }
     }
 }
--- castomhashmap/src/test/java/com/epam/java/se/CustomHashMapTest.java
+++ castomhashmap/src/test/java/com/epam/java/se/CustomHashMapTest.java
@@ -2,19 +2,24 @@ package com.epam.java.se;
 
 
 import org.junit.Before;
+import org.junit.FixMethodOrder;
 import org.junit.Test;
 
+import static org.hamcrest.CoreMatchers.equalTo;
 import static org.hamcrest.CoreMatchers.is;
 import static org.hamcrest.CoreMatchers.notNullValue;
 import static org.hamcrest.MatcherAssert.assertThat;
+import static org.junit.Assert.assertFalse;
+import static org.junit.Assert.assertTrue;
 
+@FixMethodOrder
 public class CustomHashMapTest {
 
-    CustomHashMap<Integer,String> hashMap;
+    private CustomHashMap<Integer,String> hashMap;
 
     @Before
     public void init(){
-        hashMap = new CustomHashMap<Integer, String>();
+        hashMap = new CustomHashMap<>();
     }
 
     @Test
@@ -22,6 +27,62 @@ public class CustomHashMapTest {
         assertThat(hashMap, is(notNullValue()));
     }
 
+    @Test
+    public void testThatJustInitializedMapIsEmpty(){
+        assertTrue(hashMap.isEmpty());
+    }
 
+    @Test
+    public void testThatInNewMapContainsKeyReturnsFalseForAnyElement(){
+        assertThat(hashMap.containsKey(1), is(false));
+    }
 
+    @Test
+    public void testThatInNewMapContainsValueReturnFalseForAnyElement(){
+        assertThat(hashMap.containsValue("empty"), is(false));
+    }
+
+    @Test
+    public void testThatPutMethodWorksRight(){
+        hashMap.put(1, "first");
+        hashMap.containsKey(1);
+    }
+
+    @Test(expected = NullPointerException.class)
+    public void testThatUnableToPutNullKey(){
+        hashMap.put(null, null);
+    }
+
+    @Test
+    public void testThatPutMethodAllowsToAddNullValue(){
+        hashMap.put(1, null);
+        assertThat(hashMap.isEmpty(), is(false));
+    }
+
+    @Test
+    public void testThatMethodGetReturnValueIfiItExists(){
+        assertTrue(hashMap.isEmpty());
+        hashMap.put(1, "get");
+        hashMap.put(2, "set");
+        hashMap.put(3, "pet");
+        hashMap.put(4, "ret");
+        hashMap.put(5, "met");
+        assertFalse(hashMap.isEmpty());
+        assertThat(hashMap.get(5),is(equalTo("met")));
+    }
+
+    @Test
+    public void testThatRemoveMethodWorksRight(){
+        assertTrue(hashMap.isEmpty());
+        hashMap.put(1, "get");
+        hashMap.put(2, "set");
+        hashMap.put(3, "pet");
+        hashMap.put(4, "ret");
+        hashMap.put(5, "met");
+        assertFalse(hashMap.isEmpty());
+        assertTrue(hashMap.containsKey(1));
+        assertTrue(hashMap.containsValue("get"));
+        hashMap.remove(1);
+        assertFalse(hashMap.containsValue("get"));
+    }
 }
