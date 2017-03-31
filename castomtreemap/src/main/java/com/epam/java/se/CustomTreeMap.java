02d6fec0cbc97c4f06143db71eee8008041caa67
Евгений <evgeniy.alexandrov1994@gmail.com>
2017-03-31 17:03:10 +0300

    + put, size, isEmpty, Node class

--- castomtreemap/src/main/java/com/epam/java/se/CustomTreeMap.java
+++ castomtreemap/src/main/java/com/epam/java/se/CustomTreeMap.java
@@ -1,20 +1,25 @@
 package com.epam.java.se;
 
+import java.awt.*;
 import java.util.Collection;
 import java.util.Map;
+import java.util.Objects;
 import java.util.Set;
 
 
-public class CustomTreeMap<K extends Comparable<K>, V> implements Map<K,V> {
+public class CustomTreeMap<K extends Comparable<K>, V> implements Map<K, V> {
+
+    private int size = 0;
+    private Node<K, V> root = null;
 
     @Override
     public int size() {
-        return 0;
+        return size;
     }
 
     @Override
     public boolean isEmpty() {
-        return false;
+        return size == 0;
     }
 
     @Override
@@ -34,6 +39,27 @@ public class CustomTreeMap<K extends Comparable<K>, V> implements Map<K,V> {
 
     @Override
     public V put(K key, V value) {
+        return addNode(root, key, value);
+    }
+
+    private V addNode(Node<K, V> node, K key, V value) {
+        if (node == null){
+            node = new Node<>(key, value);
+            node.setColor(Color.RED);
+            size++;
+            return null;
+        }
+        else if (node.getKey().compareTo(key) < 0){
+            addNode(node.right, key, value);
+        }
+        else if (node.getKey().compareTo(key) > 0){
+            addNode(node.left, key, value);
+        }
+        else {
+            V oldValue = node.getValue();
+            node.setValue(value);
+            return oldValue;
+        }
         return null;
     }
 
@@ -49,7 +75,8 @@ public class CustomTreeMap<K extends Comparable<K>, V> implements Map<K,V> {
 
     @Override
     public void clear() {
-
+        root = null;
+        size = 0;
     }
 
     @Override
@@ -65,5 +92,46 @@ public class CustomTreeMap<K extends Comparable<K>, V> implements Map<K,V> {
     @Override
     public Set<Entry<K, V>> entrySet() {
         return null;
+    }
+
+    private static class Node<K extends Comparable<K>, V> implements Map.Entry<K, V> {
+
+        private Node<K, V> left;
+        private Node<K, V> right;
+
+        private final K key;
+        private V value;
+        private Color color;
+
+        private Node(K key, V value) {
+            Objects.requireNonNull(key);
+            this.key = key;
+            this.value = value;
+        }
+
+        @Override
+        public K getKey() {
+            return key;
+        }
+
+        @Override
+        public V getValue() {
+            return value;
+        }
+
+        @Override
+        public V setValue(V value) {
+            V oldValue = this.value;
+            this.value = value;
+            return oldValue;
+        }
+
+        public Color getColor() {
+            return color;
+        }
+
+        public void setColor(Color color) {
+            this.color = color;
+        }
     }
 }
