19ab31790331a7c8afc43e0d9081f5281da29519
Евгений <evgeniy.alexandrov1994@gmail.com>
2017-04-01 15:32:27 +0300

    +isEmpty, size.

---
+++ customarraylist/src/test/java/com/epam/java/se/CustomArrayListTest.java
@@ -0,0 +1,34 @@
+package com.epam.java.se;
+
+import org.junit.Before;
+import org.junit.Test;
+
+import java.util.List;
+
+import static org.hamcrest.MatcherAssert.assertThat;
+import static org.hamcrest.core.Is.is;
+import static org.hamcrest.core.IsNull.notNullValue;
+import static org.junit.Assert.assertTrue;
+
+public class CustomArrayListTest {
+
+
+    private List<Integer> arrayList;
+
+    @Before
+    public void init(){
+        arrayList = new CustomArrayList<>();
+    }
+
+    @Test
+    public void testThatWeCanInitializeObject(){
+        assertThat(arrayList, is(notNullValue()));
+    }
+
+    @Test
+    public void testThatIfListHasJustInitializedIsEmpty(){
+        assertTrue(arrayList.isEmpty());
+    }
+
+
+}
